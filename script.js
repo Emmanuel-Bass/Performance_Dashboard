7a5ea03dd12df17d322667481e345e7abeea5f03
Emmanuel <talk2kvngcrownedstar@gmail.com>
2026-02-27 15:32:36 +0100

    Initial commit

--- script.js
+++ script.js
@@ -78,7 +78,8 @@ const renderTasks = () => {
         taskDelBtn.classList.add('taskDelBtn');
 
         li.append(taskName, taskDelBtn);
-        taskList.appendChild(li);
+        taskList.prepend(li);
+        taskList.scrollTo({top: 0, behavior: "smooth"});
 
         checkbox.addEventListener('change', () => {
             task.completed = checkbox.checked;
@@ -107,13 +108,67 @@ addTaskBtn.addEventListener('click', () => {
 
 renderTasks();
 
-const myFocus = document.createElement('section');
-myFocus.classList.add('focus');
+const music = document.createElement('section');
+music.classList.add('music');
+
+const myMusic = [
+    {
+        name: 'let it breathe',
+        artist: 'Gunna',
+        image: './images/on_of_wun'
+    },
+    {
+        name: 'turned your back',
+        artist: 'Gunna',
+        image: './images/AGAAC.jpeg'
+    },
+    {
+        name: '',
+        artist: '',
+        image: ''
+    }
+];
+
+myMusic.forEach((song) => {
+    const songWrapper = document.createElement('section');
+    songWrapper.classList.add('songWrapper');
+
+    const musicImg = document.createElement('img');
+    musicImg.classList.add('songImg');
+    musicImg.src = song.image;
+    musicImg.alt = song.name;
+
+    const musicNav = document.createElement('section');
+    musicNav.classList.add('musicNav');
+
+    const songName = document.createElement('h1');
+    songName.textContent = song.name;
+
+    const singer = document.createElement('h2');
+    singer.textContent = song.artist;
+
+    const musicBtns = document.createElement('p');
+    musicBtns.classList.add('musicBtns');
+
+    const musicPrev = document.createElement('button');
+    musicPrev.innerHTML = '<svg height = "20px" width = "40px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M556.2 541.6C544.2 546.6 530.5 543.8 521.3 534.7L352 365.3L352 512C352 524.9 344.2 536.6 332.2 541.6C320.2 546.6 306.5 543.8 297.3 534.7L128 365.3L128 512C128 529.7 113.7 544 96 544C78.3 544 64 529.7 64 512L64 128C64 110.3 78.3 96 96 96C113.7 96 128 110.3 128 128L128 274.7L297.4 105.4C306.6 96.2 320.3 93.5 332.3 98.5C344.3 103.5 352 115.1 352 128L352 274.7L521.4 105.3C530.6 96.1 544.3 93.4 556.3 98.4C568.3 103.4 576 115.1 576 128L576 512C576 524.9 568.2 536.6 556.2 541.6z"/></svg>';
+
+    const musicPlay = document.createElement('button');
+    musicPlay.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" height = "20px" width = ""40px> <path d="M187.2 100.9C174.8 94.1 159.8 94.4 147.6 101.6C135.4 108.8 128 121.9 128 136L128 504C128 518.1 135.5 531.2 147.6 538.4C159.7 545.6 174.8 545.9 187.2 539.1L523.2 355.1C536 348.1 544 334.6 544 320C544 305.4 536 291.9 523.2 284.9L187.2 100.9z"/></svg>';
+
+    const musicNext = document.createElement('button');
+    musicNext.textContent = 'Next';
+
+    musicBtns.append(musicPrev, musicPlay, musicNext);
+    musicNav.append(songName, singer, musicBtns);
+    songWrapper.append(musicImg, musicNav);
+    music.appendChild(songWrapper);
+});
 
 const motivate = document.createElement('section');
 motivate.classList.add('motivate');
 
-dashboard.append(time, tasks, myFocus, motivate);
+dashboard.append(time, tasks, music, motivate);
 
 const themeBtn = document.querySelector('.themeBtn');
 const toggleTheme = () => {
